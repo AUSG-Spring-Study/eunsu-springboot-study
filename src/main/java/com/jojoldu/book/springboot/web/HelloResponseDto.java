a6e6df1e1ebe6ba5c7389c714e528256da3cabf1
eunsukimme <eunsu.dev@gmail.com>
2022-09-10 18:30:49 +0900

    feat: add HelloController with dto and test code
    also added lombok

---
+++ src/main/java/com/jojoldu/book/springboot/web/HelloResponseDto.java
@@ -0,0 +1,10 @@
+package com.jojoldu.book.springboot.web;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
